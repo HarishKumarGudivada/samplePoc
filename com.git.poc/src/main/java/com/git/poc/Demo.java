ebfee7c0467195e96ffe45b7ab0c21a6573c5d6a
yh04691 <Harish.Gudivada@macys.com>
2018-07-27 12:16:20 +0530

    newbranch2commit

--- com.git.poc/src/main/java/com/git/poc/Demo.java
+++ com.git.poc/src/main/java/com/git/poc/Demo.java
@@ -3,10 +3,16 @@ package com.git.poc;
 public class Demo {
 	
 	public static void main(String[] args) {
+		System.out.println("testMessage");
 		Demo d=new Demo();
-		d.printMessage();
+		d.printMessage1();
 	}
 	
+	public void printMessage1() {
+		System.out.println("==============");
+		System.out.println("Hello India");
+		System.out.println("==============");
+	}
 	
 	public void printMessage() {
 		System.out.println("==============");
