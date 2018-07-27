563986954a728d91fc2f78628152298e4840aeb2
yh04691 <Harish.Gudivada@macys.com>
2018-07-27 11:29:11 +0530

    27072018

--- com.git.poc/src/main/java/com/git/poc/Demo.java
+++ com.git.poc/src/main/java/com/git/poc/Demo.java
@@ -3,12 +3,8 @@ package com.git.poc;
 public class Demo {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Hai Boss");
 		Demo d=new Demo();
-		System.out.println("Hai Boss");
 		d.printMessage();
-		System.out.println("How Are You.");
 	}
 	
 	
